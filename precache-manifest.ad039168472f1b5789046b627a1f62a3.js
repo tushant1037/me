2b2db4ff092ab9e9a9c9ccd235b47a0b9715aa2c
tushant1037 <42100482+tushant1037@users.noreply.github.com>
2020-09-13 12:58:02 +0530

    Updates

---
+++ precache-manifest.ad039168472f1b5789046b627a1f62a3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "fa70e3bd5109d9398ac72b08bf863d8e",
+    "url": "/me/static/media/back1.fa70e3bd.png"
+  },
+  {
+    "revision": "1caf0789ee8fdb1f0b3c",
+    "url": "/me/static/js/runtime~main.1caf0789.js"
+  },
+  {
+    "revision": "bde25a56a0c337241662",
+    "url": "/me/static/js/main.bde25a56.chunk.js"
+  },
+  {
+    "revision": "573698287b10209aaf23",
+    "url": "/me/static/js/2.57369828.chunk.js"
+  },
+  {
+    "revision": "bde25a56a0c337241662",
+    "url": "/me/static/css/main.8836ce20.chunk.css"
+  },
+  {
+    "revision": "94f2a52c0e48b5212c5e91a2004fda83",
+    "url": "/me/index.html"
+  }
+];
